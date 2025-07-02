a1e5fc46425478d1e0f09d469ed7ee0dae7d648b
dev karan <devkaran7501@gmail.com>
2025-07-01 23:43:05 -0400

    Adding chroma_db and Postgres and joining all in docker-compose

--- backend/main.py
+++ backend/main.py
@@ -3,30 +3,63 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 import uvicorn
+import os
+from dotenv import load_dotenv
+from contextlib import asynccontextmanager
+
+load_dotenv()
+
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+    '''
+        Context manager for application startup and shutdown
+    '''
+
+    print("Backend starting up...")
+
+    global DATABASE_URL, CHROMA_HOST, CHROMA_PORT, GEMINI_API_KEY
+
+    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db") # Fallback for local testing
+    CHROMA_HOST = os.getenv("CHROMA_HOST", "localhost")
+    CHROMA_PORT = int(os.getenv("CHROMA_PORT", "8001")) # Convert to int
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "") # Get Gemini API Key
+
+    print(f"Attempting to connect to Postgres at: {DATABASE_URL}")
+    print(f"Attempting to connect to ChromaDB ar: {CHROMA_HOST}:{CHROMA_PORT}")
+    print(f"Gemini API key loaded: {'Yes' if GEMINI_API_KEY else 'No'}")
+
+    yield # STARTUP phase complete
+
+    print("Backend shutting down...")
 
 app = FastAPI(
     title = "RAG system back-end API",
     description = "API for document ingestion, retrieval, and generation",
-    version = "0.0.1"
+    version = "0.0.1",
+    lifespan=lifespan
 )
 
-@app.get("/", response_class = HTMLResponse)
+@app.get('/', response_class=HTMLResponse)
 async def read_root():
     '''
-        Testing if root is running
+        Root endpoint to confirm backend is running
     '''
 
-    return """
-        <html>
-            <head>
-                <title>RAG system backend</title>
-            </head>
-            <body>
-                <h1>Welcome to RAG system back-end!</h1>
-                <p>API is running visit <a href="/docs">/docs/</a> for API documentation</p>
-            </body>
-        </html>
-"""
+    return f"""
+    <html>
+        <head>
+            <title>RAG System backend</title>
+        </head>
+        <body>
+            <h1>Welcome to RAG system backend!</h1>
+            <p>API is running</p>
+            <p>Postgresql URL: <code>{DATABASE_URL}</code></p>
+            <p>ChromaDB Host:: <code>{CHROMA_HOST}:{CHROMA_PORT}</code></p>
+            <p>Gemini API Key Loaded: <code>{'Yes' if GEMINI_API_KEY else 'No (or empty)'}</code></p>
+            <p>Visit <a href="/docs">/docs</a> for API documentation</p>
+        </body>
+    </html>
+    """
 
 @app.get("/health")
 async def health_check():
